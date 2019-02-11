a3bbfbe7921f622c1273c9b9bf0e6d6fe65868c6
KORuL <korul1@yandex.ru>
2019-02-11 23:52:57 +0300

    SocketWrapper
    Modification of hydra to work in 2x-1x and 1x-2x mode. Network layer refactoring and implementation simplification.

--- sample/android/app/src/main/java/net/korul/hbbft/DatabaseApplication.kt
+++ sample/android/app/src/main/java/net/korul/hbbft/DatabaseApplication.kt
@@ -1,8 +1,11 @@
 package net.korul.hbbft
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.FirebaseApp
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
+import io.fabric.sdk.android.Fabric
 import net.korul.hbbft.CoreHBBFT.CoreHBBFT
 
 class DatabaseApplication : Application() {
@@ -12,23 +15,17 @@ class DatabaseApplication : Application() {
 
         lateinit var mCoreHBBFT2X: CoreHBBFT
             private set
-
-//        lateinit var mCoreHBBFT: CoreHBBFT
-//            private set
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         FlowManager.init(FlowConfig.Builder(this).build())
-//        Fabric.with(this, Crashlytics())
-//        FirebaseApp.initializeApp(this)
+        Fabric.with(this, Crashlytics())
+        FirebaseApp.initializeApp(this)
 
         mCoreHBBFT2X = CoreHBBFT()
         mCoreHBBFT2X.Init(this)
-
-//        mCoreHBBFT = CoreHBBFT()
-//        mCoreHBBFT.Init(this)
 
 //        FlowManager.getDatabase(AppDatabase::class.java).reset(this)
     }
